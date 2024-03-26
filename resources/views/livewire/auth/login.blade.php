c21d1e17e7b138b45b23f3d2a31311e1d55dbd4b
Ivo Vilar <ivo@grupovoz.dev.br>
2024-03-26 11:43:12 -0300

    adjust layout login page

--- resources/views/livewire/auth/login.blade.php
+++ resources/views/livewire/auth/login.blade.php
@@ -1,4 +1,4 @@
-<x-card title="Login" shadow class="mx-auto w-[450px]">
+<x-card title="Login" shadow class="mx-auto w-[450px] bg-gray-100 dark:bg-gray-900">
 
     @if($errors->hasAny(['invalidCredentials']))
         <x-alert icon="o-exclamation-triangle" class="alert-warning mb-4">
@@ -9,12 +9,12 @@
     @endif
 
     <x-form wire:submit="login">
-        <x-input label="Email" wire:model="email"/>
+        <x-input label="Email" wire:model="email" />
         <x-input label="Password" wire:model="password" type="password"/>
         <x-slot:actions>
-            <div class="w-full flex items-center justify-between">
+            <div class="w-full flex items-center justify-center">
                 <div>
-                    <x-button label="Login" class="btn-primary" type="submit" spinner="submit"/>
+                    <x-button label="Login" class="bg-purple-200" type="submit" spinner="submit" icon="o-paper-airplane"/>
                 </div>
             </div>
         </x-slot:actions>
